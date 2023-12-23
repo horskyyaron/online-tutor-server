1a60ef10dd9c079903a7bf657a5d61f0858a2f24
Yaron <horsky.yaron@gmail.com>
2023-12-23 08:39:19 +0200

    something

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const io = new Server(server);
-
-let firstUserConnected = false;
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:*",
+    },
 });
 
+function getNumOfConnClients() {
+    return io.sockets.sockets.size;
+}
+
+function listAllClients() {
+    io.sockets.sockets.forEach((socket) => {
+        const clientId = socket.id;
+        const clientIP = socket.handshake.address;
+        console.log(`Client ID: ${clientId}, IP: ${clientIP}`);
+    });
+}
+
 io.on("connection", (socket) => {
-  socket.data.role = firstUserConnected ? "Student" : "Tutor";
-  firstUserConnected = true;
-  socket.data.ip = socket.handshake.address;
-  console.log(socket.data);
-  console.log("number of connections: " + io.engine.clientsCount);
-  socket.on("disconnect", () => {
-    console.log(`${socket.role} disconneted`);
-    console.log(`${io.engine.clientsCount} clients remain`);
-  });
+    console.log(`new connection! ${socket.id}`);
+
+    socket.data.role = getNumOfConnClients() === 1 ? "Student" : "Tutor";
+    if (socket.data.role === "Student") {
+        io.emit("handshake", { role: "student" });
+    } else {
+        io.emit("handshake", { role: "tutor" });
+    }
+
+    socket.on("disconnect", () => {
+        console.log("client disconnected");
+    });
 });
 
 server.listen(4000, () => {
-  console.log("server running at http://localhost:4000");
+    console.log("server running at http://localhost:4000");
 });
