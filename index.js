209e839c6f333dd3778c222d259bce30385d400c
Yaron <horsky.yaron@gmail.com>
2023-12-23 14:35:04 +0200

    feat(index.js): emitting text to tutor
    when recieved the event 'text change' it will broadcast all other sockets, which is the tutor, the
    updated text

--- index.js
+++ index.js
@@ -7,38 +7,43 @@ const app = express();
 app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:*",
-    },
+  cors: {
+    origin: "http://localhost:*",
+  },
 });
 
 function getNumOfConnClients() {
-    return io.sockets.sockets.size;
+  return io.sockets.sockets.size;
 }
 
 function listAllClients() {
-    io.sockets.sockets.forEach((socket) => {
-        const clientId = socket.id;
-        const clientIP = socket.handshake.address;
-        console.log(`Client ID: ${clientId}, IP: ${clientIP}`);
-    });
+  io.sockets.sockets.forEach((socket) => {
+    const clientId = socket.id;
+    const clientIP = socket.handshake.address;
+    console.log(`Client ID: ${clientId}, IP: ${clientIP}`);
+  });
 }
 
 io.on("connection", (socket) => {
-    console.log(`new connection! ${socket.id}`);
+  console.log(`new connection! ${socket.id}`);
 
-    socket.data.role = getNumOfConnClients() === 1 ? "Student" : "Tutor";
-    if (socket.data.role === "Student") {
-        socket.emit("handshake", { role: "student" });
-    } else {
-        socket.emit("handshake", { role: "tutor" });
-    }
+  const role = getNumOfConnClients() === 1 ? "student" : "tutor";
+  if (role === "student") {
+    socket.emit("handshake", { role: "student" });
+  } else {
+    socket.emit("handshake", { role: "tutor" });
+  }
 
-    socket.on("disconnect", () => {
-        console.log("client disconnected");
-    });
+  socket.on("text change", (text) => {
+    socket.broadcast.emit("text change", { updatedText: text });
+    console.log(text);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
 });
 
 server.listen(4000, () => {
-    console.log("server running at http://localhost:4000");
+  console.log("server running at http://localhost:4000");
 });
